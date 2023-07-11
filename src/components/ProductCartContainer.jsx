0f8057c83f4374a9eadd8091d07a2e1d6a6b2aed
Alan Pason <alan.pason132@gmail.com>
2023-07-11 11:14:56 +0200

    Create and style: Cart Page

--- src/components/ProductCartContainer.jsx
+++ src/components/ProductCartContainer.jsx
@@ -1,15 +1,21 @@
 import Button from "react-bootstrap/esm/Button";
+import {Link} from "react-router-dom";
 
 function ProductCartContainer(props) {
-  console.log(props.price);
   return (
     <div className="d-flex align-items-center justify-content-between p-4 border-top border-bottom" style={{height: "150px"}}>
-      <img style={({height: "100px"}, {width: "10%"})} src={props.img} alt="" />
+      <Link to={`/products/${props.id}`} style={{width: "10%"}}>
+        <img style={({height: "100px"}, {width: "100%"})} src={props.img} alt="" />
+      </Link>
       <p className="fw-bold m-0 w-25">{props.title}</p>
       <div className="d-flex align-items-center justify-content-center">
-        <Button className="text-secondary bg-white border-0 fs-4">-</Button>
-        <input type="text" className="item-quantity-input text-center" value={props.quantity} />
-        <Button className="text-secondary bg-white border-0">+</Button>
+        <Button className="text-secondary bg-white border-0 fs-4" onClick={() => props.decrement(props.id)}>
+          -
+        </Button>
+        <input type="number" className="item-quantity-input text-center" value={props.quantity} />
+        <Button className="text-secondary bg-white border-0" onClick={() => props.increment(props.id)}>
+          +
+        </Button>
       </div>
       <p className="fw-bold m-0" style={{width: "70px"}}>
         $ {props.price.toFixed(2)}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -10,6 +10,12 @@ import ProductCartContainer from "../components/ProductCartContainer";
 
 function Cart() {
   const [productsInCart, setProductsInCart] = useState(JSON.parse(localStorage.getItem("products")) || []);
+  const [delivery, setDelivery] = useState(10.95);
+
+  const numberOfItems = productsInCart.map((product) => product.quantity).reduce((total, prev) => total + prev, 0);
+
+  const costOfProducts = productsInCart.reduce((total, product) => total + product.price * product.quantity, 0);
+  const totalCost = parseFloat(costOfProducts + parseFloat(delivery)).toFixed(2);
 
   function handleDelete(id) {
     setProductsInCart((prevProducts) => {
@@ -20,8 +26,38 @@ function Cart() {
     });
   }
 
+  function increment(id) {
+    setProductsInCart((prevProducts) => {
+      const updatedQuantityProducts = prevProducts.map((product) => {
+        if (product.id === id) {
+          return {...product, quantity: product.quantity + 1};
+        }
+        return product;
+      });
+
+      localStorage.setItem("products", JSON.stringify(updatedQuantityProducts));
+      localStorage.setItem("quantity", JSON.stringify(numberOfItems + 1));
+      return updatedQuantityProducts;
+    });
+  }
+
+  function decrement(id) {
+    setProductsInCart((prevProducts) => {
+      const updatedQuantityProducts = prevProducts.map((product) => {
+        if (product.id === id && product.quantity > 1) {
+          return {...product, quantity: product.quantity - 1};
+        }
+        return product;
+      });
+
+      localStorage.setItem("products", JSON.stringify(updatedQuantityProducts));
+      localStorage.setItem("quantity", JSON.stringify(numberOfItems - 1));
+      return updatedQuantityProducts;
+    });
+  }
+
   const displayProducts = productsInCart.map((product) => {
-    return <ProductCartContainer key={product.id} id={product.id} img={product.image} title={product.title} price={product.price} handleDelete={handleDelete} />;
+    return <ProductCartContainer key={product.id} id={product.id} img={product.image} title={product.title} price={product.price} handleDelete={handleDelete} quantity={product.quantity} increment={increment} decrement={decrement} />;
   });
 
   return (
@@ -31,7 +67,7 @@ function Cart() {
           <Col md={8} className="bg-white rounded">
             <div className="d-flex p-3 align-items-center">
               <h2 className="me-auto">Shopping Cart</h2>
-              <p className="m-0">3 items</p>
+              <p className="m-0">{numberOfItems} items</p>
             </div>
 
             {displayProducts}
@@ -44,13 +80,13 @@ function Cart() {
             <h2 className="p-3">Summary</h2>
             <div className="border-top border-bottom">
               <div className="d-flex p-3">
-                <p className="me-auto">Itmes 3</p>
-                <p>$ 132.00</p>
+                <p className="me-auto">Itmes {numberOfItems}</p>
+                <p>$ {totalCost}</p>
               </div>
 
               <form className="py-2 mb-5">
                 <Form.Label htmlFor="shipping">SHIPPING:</Form.Label>
-                <Form.Select className="p-2" id="shipping" aria-label="Default select example">
+                <Form.Select onChange={(e) => setDelivery(e.target.value)} className="p-2" id="shipping" aria-label="Default select example">
                   <option value="10.95">Strandard Delivery-$10.95</option>
                   <option value="14.99">Fast Delivery-$14.99</option>
                 </Form.Select>
@@ -63,7 +99,7 @@ function Cart() {
             <div>
               <div className="d-flex p-3">
                 <p className="me-auto">TOTAL PRICE</p>
-                <p>$ 137.00</p>
+                <p>$ {totalCost}</p>
               </div>
               <Button variant="danger" className="w-100 mt-2 mb-4">
                 Checkout
